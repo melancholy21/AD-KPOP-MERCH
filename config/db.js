4d77e356bb3edcdfac529d306b66c295822cd12b
melancholy21 <angelodn1234@gmail.com>
2025-06-12 20:16:58 +0800

    New changes

--- config/db.js
+++ config/db.js
@@ -17,7 +17,7 @@ async function connectDB() {
             bufferCommands: false
         };
 
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/AD-KPOP MERCH`, opts).then(mongoose => {
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/adkpopmerch`, opts).then(mongoose => {
             return mongoose
         })
     }
--- models/User.js
+++ models/User.js
@@ -8,6 +8,6 @@ const userSchema = new mongoose.Schema({
     cartItems: { type:Object, default: {} }
 }, { minimize: false })
 
-const User = mongoose.models.user || mongoose.model('user', userSchema);
+const User = mongoose.models.user || mongoose.model('users', userSchema);
 
 export default User
