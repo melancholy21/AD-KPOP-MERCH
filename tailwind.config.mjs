6b2f15f4fde0d3a13bfa43a7545cb58a6da1ef20
melancholy21 <angelodn1234@gmail.com>
2026-05-30 20:56:30 +0800

    Generate new AD favicon and update site primary theme to purple

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,18 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        orange: {
+          50: '#faf5ff',   // purple-50
+          100: '#f3e8ff',  // purple-100
+          200: '#e9d5ff',  // purple-200
+          300: '#d8b4fe',  // purple-300
+          400: '#c084fc',  // purple-400
+          500: '#a855f7',  // purple-500 (Primary)
+          600: '#9333ea',  // purple-600
+          700: '#7e22ce',  // purple-700
+          800: '#6b21a8',  // purple-800
+          900: '#581c87',  // purple-900
+        }
       },
       gridTemplateColumns:{
         'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
